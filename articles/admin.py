4e58ff268a2b461833811c2c324cbbb9a7dc113c
jessen <ndt94hvnh@gmail.com>
2018-10-24 22:38:36 +0700

    Initial commit

---
+++ articles/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Article, Comment
+# Register your models here.
+# class CommentInline(admin.StackedInline):
+#     model = Comment
+# class ArticleAdmin(admin.ModelAdmin):
+#     inlines = [
+#         CommentInline
+#     ]
+class CommentInline(admin.TabularInline):
+    model = Comment
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline
+    ]
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
